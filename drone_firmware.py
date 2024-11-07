3ad9d50f366ea03c4801532d63fcc2ef6ef92ef0
Fancules <youriibuha@gmail.com>
2024-11-07 14:54:07 +0200

    control

--- drone_firmware.py
+++ drone_firmware.py
@@ -152,6 +152,8 @@ def start_ws():
 
 
 def drone_control(left_motor, right_motor):
+    print("LEFT_MOTOR_SPEED: ", left_motor)
+    print("RIGHT_MOTOR_SPEED: ", right_motor)
     speed_left_motor = map_value(left_motor, ETHERNET_SETTINGS.min, ETHERNET_SETTINGS.max, 4.2, 10.2)
     speed_right_motor = map_value(-right_motor, ETHERNET_SETTINGS.min, ETHERNET_SETTINGS.max, 4.2, 10.2)
 
