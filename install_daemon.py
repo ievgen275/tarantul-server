18eb1cc6a0e4bc54afa1e034282322303e24a86c
Fancules <youriibuha@gmail.com>
2025-01-20 10:43:27 +0200

    test

--- install_daemon.py
+++ install_daemon.py
@@ -4,14 +4,16 @@ import subprocess
 SERVICE_NAME = "drone.service"
 SERVICE_PATH = f"/etc/systemd/system/{SERVICE_NAME}"
 
+WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
 SERVICE_CONTENT = f"""
 [Unit]
 Description=Drone Firmware
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 -m tarantul_server.drone_firmware
-WorkingDirectory=/home/conceptarea/tarantul-server/
+ExecStart=/usr/bin/python3 -m tarantul_server.drone_firmware.py
+WorkingDirectory={WORKING_DIRECTORY}
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
@@ -29,10 +31,10 @@ def install_service():
     subprocess.run(["systemctl", "daemon-reload"], check=True)
     subprocess.run(["systemctl", "enable", SERVICE_NAME], check=True)
     subprocess.run(["systemctl", "start", SERVICE_NAME], check=True)
-    print(f"Сервіс {SERVICE_NAME} успішно встановлено та запущено.")
+    print(f"Service {SERVICE_NAME} done install and start.")
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
-        print("Будь ласка, запустіть як root.")
+        print("Start script root.")
     else:
         install_service()
